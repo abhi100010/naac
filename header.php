473a4ad68b216f1f7eda436f9f871e20dfd2f00f
abhi100010 <122353421+abhi100010@users.noreply.github.com>
2023-01-10 16:15:58 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+  <link rel="stylesheet" href="main.css">
+  <link rel="shortcut icon" href="icon.png">
+
+</head>
+<body>
+<center><a href="index.php">
+  <img id="logo" src="logo.png"></a>
+</center>
+<br>
+  <div class="navbar">
+    <a href="index.php">Home</a>
+    <a href="#about">About</a>
+    <div class="dropdown">
+      <button class="dropbtn">1
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a class="dropdown-content1.1" href="1.1front.php" title="Number of courses offered by the institution across all program during
+the year">1.1</a>
+        <a class="dropdown-content1.1.3" href="1.1.3front.php" title="Teachers of the Institution participate in following activities related to curriculum development and assessment of the affiliating University and/are represented on the following academic bodies during the year
+1. Academic council/BoS of Affiliating university
+2. Setting of question papers for UG/PG programs
+3. Design and  Development of Curriculum for Add on/ certificate/ Diploma Courses
+4. Assessment /evaluation process of the affiliating University ">1.1.3</a>
+        <a class="dropdown-content1.2.1" href="1.2.1front.php"
+          title="Number of Programmes in which Choice Based Credit System (CBCS)/ elective course system has been implemented">1.2.1</a>
+        <a class="dropdown-content1.2.2&1.2.3" href="1.2.2&1.2.3.front"
+          title="Number of Add on /Certificate programs offered during the year
+Number of students enrolled in Certificate/ Add-on programs as against the total number of students during the year">1.2.2 & 1.2.3</a>
+        <a class="dropdown-content1.3.2" href="1.3.2.front.php"
+          title=" Number of courses that include experiential learning through project work/field work/internship during the year">1.3.2</a>
+        <a class="dropdown-content1.3.3" href="1.3.3.front.php"
+          title=" Number of students undertaking project work/field work/ internships ">1.3.3</a>
+      </div>
+
+
+    </div>
+
+    <div class="dropdown">
+      <button class="dropbtn">2
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a class="dropdown-content2.1" href="#2.1" title=" Number of students during the year">2.1</a>
+        <a class="dropdown-content2.2" href="#2.2" title="Number of seats earmarked for reserved category as per GOI/ State Govt. rule during the year
+     Upload supporting document**">2.2</a>
+        <a class="dropdown-content2.3" href="#2.3"
+          title=" Number of outgoing/ final year students during the year">2.3</a>
+        <a class="dropdown-content2.1.1" href="#2.1.1" title=" Enrolment Number ">2.1.1</a>
+        <a class="dropdown-content2.1.2" href="#2.1.2"
+          title=" Number of seats filled against seats reserved for various categories (SC,   ST, OBC, Divyangjan, etc. as per applicable reservation policy during the year  (exclusive of supernumerary seats)">2.1.2</a>
+        <a class="dropdown-content2.4.1 & 2.4.3" href="#2.4.1 & 2.4.3"
+          title=" Number of full time teachers against sanctioned posts during the year
+Number of years of teaching experience of full time teachers in the same institution (Data for the latest completed academic year)">2.4.1 & 2.4.3</a>
+        <a class="dropdown-content2.4.2,3.1.2 & 3.3.1" href="#2.4.2,3.1.2 & 3.3.1" title="Number  of full time teachers with Ph. D. / D.M. / M.Ch. / D.N.B Superspeciality / D.Sc. / D.Litt. during the year (consider only highest degree for count)
+Number  of teachers recognized as research guides (latest completed academic year)
+Number of Ph.Ds registered per eligible teacher during the year">2.4.2 , 3.1.2 & 3.3.1</a>
+        <a class="dropdown-content2.6.3" href="#2.6.3" title="Pass percentage of Students during the year">2.6.3</a>
+      </div>
+
+    </div>
+    <div class="dropdown">
+      <button class="dropbtn">3
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a class="dropdown-content3.1" href="#3.1" title="a) Number of full time teachers during the year
+ b) Number of full time teachers who left/joined the institution during the year">3.1</a>
+        <a class="dropdown-content3.2" href="#3.2" title=" Number of Sanctioned posts during the year">3.2</a>
+        <a class="dropdown-content3.1.1 & 3.1.3" href="#3.1.1 & 3.1.3" title="Grants received from Government and non-governmental agencies for research projects / endowments in the institution during the year (INR in Lakhs)
+Number  of departments having Research projects funded by government and non government agencies during the year">3.1.1
+          & 3.1.3</a>
+        <a class="dropdown-content3.2.2" href="#3.2.2"
+          title="Number of workshops/seminars conducted on Research Methodology, Intellectual Property Rights (IPR) and entrepreneurship during the year">3.2.2</a>
+        <a class="dropdown-content3.3.2" href="3.3.2.front.php"
+          title="Number of research papers per teachers in the Journals notified on UGC website during the year">3.3.2</a>
+        <a class="dropdown-content3.3.3" href="#3.3.3"
+          title="Number of books and chapters in edited volumes/books published and papers published in national/ international conference proceedings per teacher during year">3.3.3</a>
+        <a class="dropdown-content3.4.2" href="#3.4.2"
+          title="Number of awards and recognitions received for extension activities from government/ government recognized bodies during the year">3.4.2</a>
+        <a class="dropdown-content3.4.3 & 3.4.4" href="#3.4.3 & 3.4.4"
+          title="Number of extension and outreach Programmes conducted by the institution through NSS/ NCC/Government and Government recognized bodies (including the programmes such as Swachh Bharat, AIDS awareness, Gender issues etc. ) and/or  those organised in collaboration with industry, community and NGOs during the year                                                                                                                 & 3.4.4 Number of students participating in extension activities at 3.4.3. above during">3.4.3
+          & 3.4.4</a>
+        <a class="dropdown-content3.5.1" href="#3.5.1"
+          title="Number of Collaborative activities for  research, Faculty exchange, Student exchange/ internship during the year">3.5.1</a>
+        <a class="dropdown-content3.5.2" href="#3.5.2"
+          title="Number of functional MoUs with institutions, other universities, industries, corporate houses etc. during the year">3.5.2</a>
+      </div>
+    </div>
+    <div class="dropdown">
+      <button class="dropbtn">4
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a class="dropdown-content4.1.3" href="#4.1.3"
+          title="Number of classrooms and seminar halls with ICT- enabled facilities such as smart class, LMS, etc.">4.1.3</a>
+        <a class="dropdown-content4.1.4 & 4.4.1" href="#4.1.4 & 4.4.1"
+          title=" Expenditure, excluding salary for infrastructure augmentation during the year (INR in Lakhs) & 4.4.1 Expenditure incurred on maintenance of infrastructure (physical and academic support facilities) excluding salary component during the year (INR in Lakhs)">4.1.4
+          & 4.4.1</a>
+        <a class="dropdown-content4.2.2 & 4.2.3" href="#4.2.2 & 4.2.3"
+          title="The institution has subscription for the following e-resources
+1. e-journals, 2. e-ShodhSindhu, 3. Shodhganga membersip, 4. e-books, 5. Databases, 6. Remote access to e- resources
+Expenditure for purchase of books/e-books and subscription to journals/e- journals during the year (INR in Lakhs)">4.2.2 & 4.2.3</a>
+      </div>
+    </div>
+    <div class="dropdown">
+      <button class="dropbtn">5
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a class="dropdown-content5.1.1 & 5.1.2" href="#5.1.1 & 5.1.2"
+          title="Number of students benefited by scholarships and free ships provided by the Government during the year
+Number of students benefitted by scholarships, free ships etc. provided by the institution / non- government bodies, industries, individuals, philanthropists during the year">5.1.1
+          & 5.1.2</a>
+        <a class="dropdown-content5.1.3" href="5.1.3.front.php"
+          title="Capacity building and skills enhancement initiatives taken by the institution include the following
+1. Soft skills, 2. Language and communication skills, 3. Life skills (Yoga, physical fitness, health and hygiene), 4. ICT/computing  skills">5.1.3</a>
+        <a class="dropdown-content5.1.4" href="5.1.4front.php"
+          title="Number of students benefitted by guidance for competitive examinations and career counseling offered by the Institution during the year">5.1.4</a>
+        <a class="dropdown-content5.2.1" href="5.2.1.front.php"
+          title="Number of placement of outgoing students during the year">5.2.1</a>
+        <a class="dropdown-content5.2.2" href="#5.2.2"
+          title="Number of students progressing to higher education during the year">5.2.2</a>
+        <a class="dropdown-content5.2.3" href="5.2.3.front.php"
+          title="Number of students qualifying in state/national/ international level examinations during the year (eg: JAM/CLAT/GATE/ GMAT/CAT/GRE/ TOEFL/ Civil Services/State government examinations)">5.2.3</a>
+        <a class="dropdown-content5.3.1" href="5.3.1.front.php"
+          title="Number of awards/medals for outstanding performance in sports/cultural activities at university/state/national / international level (award for a team event should be counted as one) during the year">5.3.1</a>
+        <a class="dropdown-content5.3.3" href="5.3.3.front.php"
+          title="Number of sports and cultural events/competitions in which students of the Institution participated during the year (organized by the institution/other institutions)">5.3.3</a>
+      </div>
+    </div>
+    <div class="dropdown">
+      <button class="dropbtn">6
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a class="dropdown-content6.2.3" href="#6.2.3" title="Implementation of e-governance in areas of operation
+ 1. Administration, 2. Finance and Accounts, 3. Student Admission and Support, 4. Examination">6.2.3</a>
+        <a class="dropdown-content6.3.2" href="#6.3.2"
+          title="Number of teachers provided with financial support to attend conferences/ workshops and towards membership fee of professional bodies during the year">6.3.2</a>
+        <a class="dropdown-content6.3.3" href="6.3.3.html" title="Number of professional development /administrative training programs organized by the institution for teaching and non-teaching staff during the year
+">6.3.3</a>
+        <a class="dropdown-content6.3.4" href="6.3.4.html"
+          title="Number of teachers undergoing online/face-to-face Faculty development Programmes (FDP) during the year
+(Professional Development Programmes, Orientation / Induction Programmes, Refresher Course, Short Term Course etc.)">6.3.4</a>
+        <a class="dropdown-content6.4.2" href="#6.4.2"
+          title="Funds / Grants received from non-government bodies, individuals, philanthropers during the year (not covered in Criterion III)">6.4.2</a>
+        <a class="dropdown-content6.5.3" href="#6.5.3" title="Quality assurance initiatives of the institution include:
+1. Regular meeting of Internal Quality Assurance Cell (IQAC);  Feedback collected, analysed and used for improvements
+2.  Collaborative quality intitiatives with other institution(s)
+3. Participation in NIRF
+4 .any other quality audit recognized by state, national or international agencies (ISO Certification, NBA)">6.5.3</a>
+      </div>
+    </div>
+    <div class="dropdown">
+      <button class="dropbtn">Activity
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+      <a class="dropdown-content6.2.3" href="culturalfront.php" title="Cultural Activities">Cultural Activities</a>
+      <a class="dropdown-content6.2.3" href="nssfront.php" title="NSS Activities">NSS Activities</a>
+      <a class="dropdown-content6.2.3" href="sportsfront.php" title="Sports Activities">Sports Activities</a>
+      <a class="dropdown-content6.2.3" href="edcfront.php" title="EDC Activities">EDC Activities</a>
+      <a class="dropdown-content6.2.3" href="ccfront.php" title="Career Counselling Activities">Career Counselling Activities</a>
+
+    </div>
+  </div>
+  </div>
+</body>
+
+</html>
